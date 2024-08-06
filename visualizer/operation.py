96bb598d60ec8713fc8c0b961b20d50cf7b99c7d
arianfiroozi <arianfiroozi3@gmail.com>
2024-08-06 04:15:52 -0700

    structured into files

---
+++ visualizer/operation.py
@@ -0,0 +1,100 @@
+import torch
+from graphviz import Digraph
+import pickle
+import torch.nn as nn
+from enum import Enum
+
+class OperationType(Enum):
+    INPUT=1
+    MAC=2
+    ADD=3
+    MULT=4
+    OUTPUT=5
+    POOL=6
+    PADD=7
+    DILATION=8
+    CONST=9
+    UNKNOWN=0
+
+class Operation: # node
+    def __init__(self, name:str, type:OperationType=0, label:str="OP"):
+        self.name=name # unique
+        self.label=label
+        self.type=type
+        self.inputs=[]
+    
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return isinstance(other, Operation) and self.name == other.name
+
+    def __repr__(self):
+        return f"Operation(name={self.name},\n type={self.type})"
+
+    def get_label(self):
+        return self.label
+
+    def add_input_name(self, name:str):
+        self.inputs.append(name)
+    
+    def get_name(self)->str:
+        return self.name
+
+class PaddOP(Operation):
+    def __init__(self, name:str, padding:list, label:str="Padding"):
+        super().__init__(name, OperationType.PADD, label)
+        self.padding = padding
+
+    def get_label(self):
+        return self.label + ": " + str(self.padding)
+
+class DilationOP(Operation):
+    def __init__(self, name:str, dilation:list, label:str="Dilation"):
+        super().__init__(name, OperationType.DILATION, label)
+        self.dilation = dilation
+
+    def get_label(self):
+        return self.label + ": " + str(self.dilation)
+
+class MacOP(Operation):
+    def __init__(self, name:str, input_index, weight_index, label:str="MAC"):
+        super().__init__(name, OperationType.MAC, label)
+        self.input_index=input_index
+        self.weight_index=weight_index
+
+    def get_label(self):
+        printable = self.label + ":" 
+        printable += "\nX" + str(self.input_index) 
+        printable += "\nW" + str(self.weight_index)
+        return printable
+
+class ConstOP(Operation):
+    def __init__(self, name:str, shape:list, label:str="Const"):
+        super().__init__(name, OperationType.CONST, label)
+        self.shape = shape
+
+    def get_label(self):
+        printable = self.label + ":" 
+        printable += "\nW" + str(self.shape)
+        return printable
+
+class InputOP(Operation):
+    def __init__(self, name:str, shape:list, label:str="Input"):
+        super().__init__(name, OperationType.INPUT, label)
+        self.shape = shape
+
+    def get_label(self):
+        printable = self.label + ":" 
+        printable += "\nX" + str(self.shape)
+        return printable
+
+class OutputOP(Operation):
+    def __init__(self, name:str, shape:list, label:str="Output"):
+        super().__init__(name, OperationType.OUTPUT, label)
+        self.shape = shape
+
+    def get_label(self):
+        printable = self.label + ":" 
+        printable += "\nY" + str(self.shape)
+        return printable
